47424f2801c40abb68944c48c8ed3bc1f1adce07
Dani2601 <danielvitalista811@gmail.com>
2026-01-05 21:01:52 +0800

    Added Tailwind + PostCSS config for ESM Vite project

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="text-3xl font-bold text-blue-600">
+        Tailwind is working!
+      </div>
     </>
   )
 }
